8452afeafd7aa1eecc6be213244f1ab078b647ee
titi <thaophuong5625@gmail.com>
2023-08-21 16:05:34 +0200

    Add ThreeSum

--- src/main/java/hard/ThreeSum.java
+++ src/main/java/hard/ThreeSum.java
@@ -1,4 +1,59 @@
 package hard;
 
+import java.util.*;
+
+/*
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k,
+and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+ */
 public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1,0,1,2,-1,-4};
+        System.out.println(threeSum(nums));
+    }
+
+    private static List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> setInt = new HashSet<>();
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    setInt.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return new ArrayList<>(setInt);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
