ca2778d32ab2210a1064ad808b07e3b567225209
titi <thaophuong5625@gmail.com>
2023-08-30 11:28:11 +0200

    Add QueueCourses

--- src/main/java/hard/Test.java
+++ src/main/java/hard/Test.java
@@ -20,73 +20,8 @@ public class Test {
 
 
 
-//    public int evalRPN(String[] tokens) {
-//        Stack<Integer> stack = new Stack<>();
-//        for(int i = 0; i < tokens.length; i++){
-//            if(tokens[i].equals("+")){
-//                int b = stack.pop();
-//                int a = stack.pop();
-//                stack.push(a+b);
-//            }
-//            else if(tokens[i].equals("-")){
-//                int b = stack.pop();
-//                int a = stack.pop();
-//                stack.push(a-b);
-//            }
-//            else if(tokens[i].equals("*")){
-//                int b = stack.pop();
-//                int a = stack.pop();
-//                stack.push(a*b);
-//            }
-//            else if(tokens[i].equals("/")){
-//                int b = stack.pop();
-//                int a = stack.pop();
-//                stack.push(a/b);
-//            }
-//            else
-//                stack.push(Integer.parseInt(tokens[i]));
-//        }
-//        return stack.pop();
-//    }
 
-//    public boolean canFinish(int n, int[][] prerequisites) {
-//        List<Integer>[] adj = new List[n];
-//        int[] indegree = new int[n];
-//        List<Integer> ans = new ArrayList<>();
-//
-//        for (int[] pair : prerequisites) {
-//            int course = pair[0];
-//            int prerequisite = pair[1];
-//            if (adj[prerequisite] == null) {
-//                adj[prerequisite] = new ArrayList<>();
-//            }
-//            adj[prerequisite].add(course);
-//            indegree[course]++;
-//        }
-//
-//        Queue<Integer> queue = new LinkedList<>();
-//        for (int i = 0; i < n; i++) {
-//            if (indegree[i] == 0) {
-//                queue.offer(i);
-//            }
-//        }
-//
-//        while (!queue.isEmpty()) {
-//            int current = queue.poll();
-//            ans.add(current);
-//
-//            if (adj[current] != null) {
-//                for (int next : adj[current]) {
-//                    indegree[next]--;
-//                    if (indegree[next] == 0) {
-//                        queue.offer(next);
-//                    }
-//                }
-//            }
-//        }
-//
-//        return ans.size() == n;
-//    }
+
 
 
 
