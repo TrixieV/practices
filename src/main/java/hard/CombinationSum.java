5c17c78fca86dbcacdfa9f9cb4113afcdddaf9c3
titi <thaophuong5625@gmail.com>
2023-09-01 11:59:58 +0200

    Add CombinationSum

---
+++ src/main/java/hard/CombinationSum.java
@@ -0,0 +1,47 @@
+package hard;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Given an array of distinct integers candidates and a target integer target,
+return a list of all unique combinations of candidates where the chosen numbers sum to target.
+You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency
+of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations
+that sum up to target is less than 150 combinations for the given input.
+
+
+ */
+public class CombinationSum {
+    public static void main(String[] args) {
+        int[] candidates = new int[]{4,5,6,7,8,9};
+        System.out.println(combinationSum(candidates, 20));
+    }
+
+    private static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> ans = new ArrayList<>();
+        checkingAndUpdateLists(candidates, ans, new ArrayList<>(), 0, target);
+        return ans;
+    }
+
+    private static void checkingAndUpdateLists(int[] candidates, List<List<Integer>> ans,
+                                               List<Integer> list, int start, int remaining) {
+        if (remaining < 0) {
+            return;
+        } else if (remaining == 0) {
+            ans.add(new ArrayList<>(list));
+        } else {
+            for (int i = start; i < candidates.length; i++) {
+                list.add(candidates[i]);
+                checkingAndUpdateLists(candidates, ans, list, i, remaining - candidates[i]);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
