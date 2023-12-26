d8a6315a4e4f5ae053ead5f75cf9d5bbe69b46aa
titi <thaophuong5625@gmail.com>
2023-12-26 13:41:25 +0700

    Add Practices

---
+++ src/main/java/moderate2/StringReverse.java
@@ -0,0 +1,73 @@
+package moderate2;
+
+import java.util.Stack;
+
+/*
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words.
+The returned string should only have a single space separating the words. Do not include any extra spaces.
+ */
+public class StringReverse {
+    public static void main(String[] args) {
+        String s = " Hello   my  name  is    Thao   ";
+        System.out.println(reverseWords2(s));
+    }
+
+    private static String reverseWords2(String s) {
+        StringBuilder ans = new StringBuilder();
+        int start = s.length() - 1, end = s.length();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                start = i;
+                if (i == 0) {
+                    ans.append(s, start, end);
+                    break;
+                }
+            } else {
+                if (!s.substring(start, end).equals(" ")) {
+                    ans.append(s, start, end);
+                    ans.append(" ");
+                }
+                for (int j = i - 1; j >= 0; j--) {
+                    if (s.charAt(j) != ' ') {
+                        i = j + 1;
+                        break;
+                    }
+                    i = j;
+                }
+                end = i;
+            }
+        }
+        if (ans.substring(ans.length() - 1).equals(" ")) {
+            ans.deleteCharAt(ans.length() - 1);
+        }
+
+        return ans.toString();
+    }
+
+    private static String reverseWords(String s) {
+        Stack<String> words = new Stack<>();
+        String[] allWords = s.split(" ");
+        for (String word : allWords) {
+            if (!word.isEmpty()) {
+                words.add(word);
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+        while (!words.isEmpty()) {
+            ans.append(words.pop());
+            if (words.isEmpty()) {
+                continue;
+            }
+            ans.append(" ");
+        }
+
+        return ans.toString();
+    }
+}
